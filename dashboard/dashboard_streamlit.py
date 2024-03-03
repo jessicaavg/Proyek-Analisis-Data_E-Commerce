aad66fa709587d777cce66ba750ceacc6d3f4a9b
Jessica Vega_21.11.4356 <97297900+jessicaavg@users.noreply.github.com>
2024-03-04 00:03:52 +0700

    Update dashboard_streamlit.py

--- dashboard/dashboard_streamlit.py
+++ dashboard/dashboard_streamlit.py
@@ -45,7 +45,7 @@ data_sellers = pd.read_csv('data/sellers_dataset.csv')
 ##          SIDEBAR          ##
 ###############################
 st.sidebar.image('dashboard/analytics_data.png')
-st.subheader('What do you want to explore?')
+
 data_customer = pd.read_csv('data/customers_dataset.csv')
 st.sidebar.button("View Data")
 
